ca4a84309920192e9b80314ec9f83df0c066df1c
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-07 23:48:34 +0700

    Kondisi IF ELSE
    Ada tiga pengkondisian dengan IF ELSE

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -21,6 +21,21 @@
         <h1 class="bg-dark px-3 py-1 text-white d-inline-block">
             {{ date(now()) }}
         </h1>
+        <br>
+        {{-- Kondisi IF ELSE --}}
+        @if ($nilai > 0 and $nilai < 50)
+            <div class="alert alert-danger d-inline-block">
+                Maaf, Anda tidak lulus.
+            </div>
+        @elseif ($nilai >= 50 and $nilai <= 100)
+            <div class="alert alert-success d-inline-block">
+                Selamat, Anda lulus.
+            </div>
+        @else
+            <div class="alert alert-dark d-inline-block">
+                Nilai tidak valid.
+            </div>
+        @endif
     </div>
 </body>
 
