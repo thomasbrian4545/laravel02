1dca23c00822290ec123705b80841a3eb53dfa5f
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-09 23:55:31 +0700

    Include 'layout.header' dan 'layout.footer'
    Di kode program direksi.blade.php

--- resources/views/direksi.blade.php
+++ resources/views/direksi.blade.php
@@ -1,49 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <title>Data Direksi</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('pegawai') }}">Data Pegawai</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('direksi') }}">Data Direksi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('gallery') }}">Gallery</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div class="container text-center mt-3 p-4 bg-white">
-        <h1 class="mb-3">Data Direksi</h1>
-        <div class="row">
-            <div class="col-sm-8 col-md-6 m-auto">
-                <ol class="list-group">
-                    @forelse ($direksi as $val)
-                        <li class="list-group-item">{{ $val }}</li>
-                    @empty
-                        <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
-                    @endforelse
-                </ol>
-            </div>
+@include('layout.header')
+<div class="container text-center mt-3 p-4 bg-white">
+    <h1 class="mb-3">Data Direksi</h1>
+    <div class="row">
+        <div class="col-sm-8 col-md-6 m-auto">
+            <ol class="list-group">
+                @forelse ($direksi as $val)
+                    <li class="list-group-item">{{ $val }}</li>
+                @empty
+                    <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
+                @endforelse
+            </ol>
         </div>
     </div>
-    <footer class="bg-dark py-4 text-white mt-4">
-        <div class="container">
-            Sistem Informasi | Copyright {{ date('Y') }} Belajarlaravel
-        </div>
-    </footer>
-</body>
-
-</html>
+</div>
+@include('layout.footer')
